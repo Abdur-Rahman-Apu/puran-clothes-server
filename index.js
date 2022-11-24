aa4a1ab1a6ec62d3a6bc63f18be3202f81866f1e
Abdur Rahman Apu <abdurrahmany418@gmail.com>
2022-11-24 23:40:53 +0600

    mongoDB setup is done

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app = express()
 
 const cors = require('cors');
 
+require('dotenv').config()
 app.use(cors())
 app.use(express.json())
 
@@ -13,6 +14,21 @@ const port = process.env.PORT || 5000
 app.get('/', async (req, res) => {
     res.send("Server is running")
 })
+
+
+
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.7kbtzra.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run() {
+
+}
+run().catch(error => console.log(error))
 
 app.listen(port, () => {
     console.log("Server is running on port ", port);
